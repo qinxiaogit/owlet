07a1517a30fe4aa4b5638de832540ad9649266e3
qinxiaogit <qinxiaolove@gmail.com>
2017-09-23 23:27:35 +0800

    测试headers

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 
 $http = new swoole_http_server("0.0.0.0", 80);
 $http->on('request', function ($request, $response) {
+	$response->write($request->headers
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 $http->start();
