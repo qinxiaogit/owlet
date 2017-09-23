411ecd754528418bb6955fc93e9097e9a48bc955
qinxiaogit <qinxiaolove@gmail.com>
2017-09-23 23:03:45 +0800

    测试httpserver

---
+++ init.php
@@ -0,0 +1,5 @@
+$http = new swoole_http_server("0.0.0.0", 80);
+$http->on('request', function ($request, $response) {
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+});
+$http->start();
